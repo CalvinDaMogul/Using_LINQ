e288ded4858e000476080002b24304b3c6551155
Calvin J Foster <cal.j.foster@gmail.com>
2018-10-08 20:01:33 -0500

    another commit

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace LINQ
     {
         // The Three Parts of a LINQ Query:
         //  1. Data source.
-        int[] numbers = new int[21] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int[] numbers = new int[25] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
 
         // 2. Query creation.
         // numQuery is an IEnumerable<int>
