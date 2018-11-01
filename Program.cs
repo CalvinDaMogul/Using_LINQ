9983daea515f8873da7709bca12eacbf57138d25
Calvin J Foster <cal.j.foster@gmail.com>
2018-10-31 20:35:24 -0500

    new commit

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ namespace LINQ
         string[] cars = {"Camaro", "BMW", "Mercedes", "Bentley", "Hummer", "i8", "RangeRover", "Beamer","Cadillac","Rolls Royce"};
 		
       //Get only short words
-      var carNames = from car in cars where car.Length >= 5 select car;
+      var carNames = from car in cars where car.Length >= 3 select car;
 	    
       //Print each word out
       foreach (var car in carNames) {
